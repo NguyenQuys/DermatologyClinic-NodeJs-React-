cef7cdfad87c70c49cedfc6f11065f923e27e256
NguyenQuys <nguyenhuuquy110203@gmail.com>
2025-04-21 11:34:24 +0700

    finnish

--- BE/src/controllers/auth.controller.js
+++ BE/src/controllers/auth.controller.js
@@ -7,7 +7,7 @@ exports.register = async (req, res) => {
 
     res.status(201).json({ message: "Đăng ký thành công!" });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(error.status).json({ message: error.message });
   }
 };
 
